c09a28526236a5485e5cfda773908882e9f8a5d4
partho1999 <sarker624452@gmail.com>
2025-09-09 23:02:53 +0600

    fix in webhook-view

--- payments/urls.py
+++ payments/urls.py
@@ -3,12 +3,12 @@ from .views import (
     CreatePaymentIntentView,
     ShopOnboardingLinkView,
     SaveCardView,
-    stripe_webhook
+    StripeWebhookView
 )
 
 urlpatterns = [
     path("payment-intent/<int:booking_id>/", CreatePaymentIntentView.as_view(), name="payment-intent"),
     path("shop-onboarding/<int:shop_id>/", ShopOnboardingLinkView.as_view(), name="shop-onboarding"),
     path("save-card/", SaveCardView.as_view(), name="save-card"),
-    path("stripe-webhook/", stripe_webhook, name="stripe-webhook"),
+    path("stripe-webhook/", StripeWebhookView, name="stripe-webhook"),
 ]
